bf7dcb910a8dbd04d6da53332b4fe12cf8ec2206
Aleksandr Moiseev <m.alex6577@mail.ru>
2023-06-20 22:58:03 +0300

    Add test

--- src/test/java/FormsTest.java
+++ src/test/java/FormsTest.java
@@ -1,5 +1,4 @@
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.api.Test;
 import service.Forms;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,11 +6,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FormsTest {
 
-    @ParameterizedTest
-    @NullSource
-    public void execute(String title) {
-        assertNull(Forms.getTitle());
+    @Test
+    public void executeTitle1_Selection1() {
+        Forms.execute("Ты потерял память", "selection1");
+        String titleTest = Forms.getTitle();
+        String text1 = Forms.getText1();
+        String text2 = Forms.getText2();
+        String location = Forms.getLocation();
+        assertEquals("Ты принял вызов. Поднимешься на мостик к их капитану?", titleTest);
+        assertEquals("Подняться на мостик", text1);
+        assertEquals("Отказаться подниматься на мостик", text2);
+        assertEquals("/mySetServlet", location);
     }
 
+    @Test
+    public void executeTitle1_Selection2() {
+        Forms.execute("Ты потерял память", "selection2");
+        String titleTest = Forms.getTitle();
+        String location = Forms.getLocation();
+        assertEquals("Ты отклонил вызов. Поражение", titleTest);
+        assertEquals("/lose", location);
+    }
 
+    @Test
+    public void executeTitle2_Selection1() {
+        Forms.execute("Ты принял вызов", "selection1");
+        String titleTest = Forms.getTitle();
+        String text1 = Forms.getText1();
+        String text2 = Forms.getText2();
+        String location = Forms.getLocation();
+        assertEquals("Ты поднялся на мостик. Ты кто?", titleTest);
+        assertEquals("Рассказать о себе правду", text1);
+        assertEquals("Солгать о себе", text2);
+        assertEquals("/mySetServlet", location);
+    }
+
+    @Test
+    public void executeTitle2_Selection2() {
+        Forms.execute("Ты принял вызов", "selection2");
+        String titleTest = Forms.getTitle();
+        String location = Forms.getLocation();
+        assertEquals("Ты не прошел переговоры... Поражение!", titleTest);
+        assertEquals("/lose", location);
+    }
+
+    @Test
+    public void executeTitle3_Selection1() {
+        Forms.execute("Ты поднялся на мостик", "selection1");
+        String titleTest = Forms.getTitle();
+        String text1 = Forms.getText1();
+        String text2 = Forms.getText2();
+        String location = Forms.getLocation();
+        assertEquals("Тебя вернули домой. Победа!", titleTest);
+        assertEquals("/lose", location);
+    }
+
+    @Test
+    public void executeTitle3_Selection2() {
+        Forms.execute("Ты поднялся на мостик", "selection2");
+        String titleTest = Forms.getTitle();
+        String location = Forms.getLocation();
+        assertEquals("Твою ложь разоблачили. Поражение", titleTest);
+        assertEquals("/lose", location);
+    }
 }
